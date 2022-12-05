d880a6002bc0527aa1bb35e75037e255e14fb080
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-12-05 14:25:48 +0100

    first

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js"></script>
+  <script src='https://unpkg.com/vue@3'></script>
+  <title>Spotify - Dischi</title>
+</head>
+<body>
+  <div id="app">
+  <h1>prova</h1>
+  </div>
+  
+
+  <script src="js/script.js"></script>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,16 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+    
+  }
+
+}).mount('#app');
